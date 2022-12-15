470649cdd3cb69cb95906a99f2904ae8f084b667
Jonathan Girón Rodríguez <43244571+srGironR@users.noreply.github.com>
2022-12-15 18:57:15 -0500

    feat(getPeople): Creada función de obtener datos de swapi y retornar el objeto

--- src/server/endpoints/swapiEndpoints.js
+++ src/server/endpoints/swapiEndpoints.js
@@ -7,8 +7,24 @@ const applySwapiEndpoints = (server, app) => {
         res.send(data);
     });
 
-    server.get('/hfswapi/getPeople/:id', async (req, res) => {
-        res.sendStatus(501);
+    server.get('/hfswapi/getPeople/:id', async (req, res) => {        
+        
+        const stwpersonW = await app.swapiFunctions.genericRequest(`https://swapi.dev/api/people/${req.params.id}`, 'GET', null, true);
+        const stwplanet = await app.swapiFunctions.genericRequest(`${stwpersonW.homeworld}`, 'GET', null, true);
+        let str = stwpersonW.homeworld.split("/");
+        //If dont get any send 400
+        if(stwpersonW.length == 0)
+         return res.status(400).send({msg:"No se encontró un personaje"});
+        //If get any send 200 
+       const resFindstperson = {
+        name:stwpersonW.name, 
+        mass:stwpersonW.mass, 
+        height:stwpersonW.height, 
+        homeworld_name:stwplanet.name, 
+        homeworld_id:str[5]
+        };
+
+        res.status(201).send(resFindstperson);
     });
 
     server.get('/hfswapi/getPlanet/:id', async (req, res) => {
